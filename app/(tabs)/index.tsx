a74d5e36c1a7fa449627b225af72f36a1575fb48
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-02-18 14:20:39 +0300

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text className="text-4xl text-blue-500">Hi</Text>
+      </SafeAreaView>
+    </>
+  );
+}
