b271de2fa2f32f157d9df127518b25bb23fd161b
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-02-22 15:07:25 +0300

    feat(constant):add filters & categories constants

---
+++ constants/Categories.ts
@@ -0,0 +1,36 @@
+export const categories = [
+  {
+    id: 1,
+    title: "All",
+  },
+  {
+    id: 2,
+    title: "Food",
+    limit: 500,
+  },
+  {
+    id: 3,
+    title: "Grocerie",
+    limit: 400,
+  },
+  {
+    id: 4,
+    title: "Travel",
+    limit: 200,
+  },
+  {
+    id: 5,
+    title: "Utilities",
+    limit: 3200,
+  },
+  {
+    id: 6,
+    title: "Health",
+    limit: 900,
+  },
+  {
+    id: 7,
+    title: "Entartainment",
+    limit: 300,
+  },
+];
---
+++ constants/Filters.ts
@@ -0,0 +1,5 @@
+export const filters = [
+    { label: "Last 1 Month", value: "1month" },
+    { label: "Last 3 Month", value: "3months" },
+    { label: "Last 6 Month", value: "6months" },
+  ];
