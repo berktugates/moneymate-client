d8d2581082b2aa9b7a32517fc9695b7a3c95497e
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-02-22 14:59:47 +0300

    feat: add income & spending interfaces

---
+++ models/IPayment.ts
@@ -0,0 +1,15 @@
+export interface ISpending {
+  id:number;
+  name: string;
+  category: string;
+  company: string;
+  price: number;
+  date: Date;
+}
+
+export interface IIncome {
+  id:number;
+  source: string;
+  amount: number;
+  date: Date;
+}
